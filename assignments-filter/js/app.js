b4c924da5b9b8e18ede421444cb3f71a69d2db3b
Indra Bijay <indrabijay007@gmail.com>
2016-04-21 21:50:11 -0400

    Assignment1Angular

---
+++ assignments-filter/js/app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by indrabijaynarayan on 4/20/16.
+ */
+angular.module('app',[])
+    .controller('appCtrl',['$scope',function($scope){
+
+    }])
+    .filter("TitleCase",function(){
+        return function(str){
+
+            var newstr = str.split(" ");
+            for(i=0;i<newstr.length;i++){
+                var copy = newstr[i].substring(1).toLowerCase();
+                newstr[i] = newstr[i][0].toUpperCase() + copy;
+            }
+            newstr = newstr.join(" ");
+            return newstr;
+            
+        };
+    })
